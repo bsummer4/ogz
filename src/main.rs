64a8203139d423f86c83a3921e6e07749c30d8d2
Benjamin Summers <ben@benjaminsummers.net>
2015-01-10 19:23:16 -0800

    Generate some cool shit.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate noise;
+
+use noise::{simplectic2, simplectic3, simplectic4, Seed, Point2};
+
+fn render(x:u8) -> char {
+  match x / 53 {
+    0 => ' ',
+    1 => ' ',
+    2 => '▩',
+    3 => '█',
+    _ => '█' }}
+
 fn main() {
-    println!("Hello, world!");
-}
+  for i in range(0, 25) {
+    for j in range(0, 50) {
+      let x = (i*4) as f32;
+      let y = (j*4) as f32;
+      let v = simplectic2(&Seed::new(9), &[x, y]);
+      let r = (128.0 + v*256.0) as u8;
+      print!("{}", render(r)); }
+    println!(""); }}

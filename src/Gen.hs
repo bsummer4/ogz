9627c8385c5735a7d77406a056720e1a8ce5c273
Benjamin Summers <ben@benjaminsummers.net>
2015-05-28 09:31:03 -0700

    Rework the Gen.hs API.

--- src/Gen.hs
+++ src/Gen.hs
@@ -1,12 +1,15 @@
 {-# LANGUAGE TypeOperators #-}
 
-module Gen
-  (roomOGZ, heightMap, pillars, maze3d, treeOctN, maze2d',leaves,stripes)
+module Gen (
+  BitField2D, BitField3D, HeightMap, SauerbratenMap,
+  Cube, Entity,
+  empty, solid, entities,
+  floorPattern, voxelPattern, heightMap, sauerbraten)
   where
 
-import Types
 import Orphans()
 import HomoTuple
+import qualified Types as Sauer
 
 import Prelude hiding (concat, mapM, mapM_, sum)
 
@@ -15,39 +18,85 @@ import           Data.Array.Repa     ((:.) (..), Array, D, DIM2, DIM3, Z (Z))
 import qualified Data.Array.Repa     as A
 import           Data.Bits
 import           Data.Foldable
-import qualified Data.List           as L
-import           Data.Monoid
-import           Debug.Trace
-import           Numeric.Noise
+import           Data.Traversable
 import           Prelude.Unicode
 import           Text.Printf
 
 
 -- Types -----------------------------------------------------------------------
 
-type BitField2d r = Array r DIM2 Bool
-type BitField3d r = Array r DIM3 Bool
+type BitField2D r = Array r DIM2 Bool
+type BitField3D r = Array r DIM3 Bool
+type HeightMap r = Array r DIM2 Int
 
-data Tree a = Branch (LzTup8 (Tree a))
-            | Leaf a
-  deriving (Show,Eq,Ord,Functor)
+type SauerbratenMap = Sauer.OGZ
+type Cube = Tree Bool
 
-type HeightMap2D = Array D ((Z :. Int) :. Int) Int
+data Entity
+
+data Tree a = Branch !(LzTup8 (Tree a))
+            | Leaf !a
+  deriving (Show,Eq,Ord,Functor,Foldable,Traversable)
+
+
+
+-- Basics ----------------------------------------------------------------------
+
+empty,solid ∷ Cube
+empty = Leaf False
+solid = Leaf True
+
+entities ∷ Cube → [Entity]
+entities = const []
+
+
+-- Conversion ------------------------------------------------------------------
+
+-- TODO Don't hard-code D! Why can't this work for any internal representation?
+floorPattern ∷ BitField2D D → Cube
+floorPattern bf = f3 <$> indexTree depth
+  where (Z:.w:.h, f2) = A.toFunction bf
+        f3 (Z :. x :. y :. z) | z≡bottom = f2 (Z :. x :. y)
+        f3 (Z :. _ :. _ :. _)            = False
+        errmsg = "2D bit fields must be squares with sizes that arepowers of two."
+        bottom = (depth*depth) - 1
+        depth = case (depthNeeded w, w≡h) of
+                  (Just d, True) → d
+                  _              → error errmsg
+
+-- TODO Don't hard-code D! Why can't this work for any internal representation?
+voxelPattern ∷ BitField3D D → Cube
+voxelPattern bf = f <$> indexTree depth
+  where (Z:.w:.h:.d, f) = A.toFunction bf
+        errmsg = "3d bit fields must be cubes with sizes that are powers of two. Not %s"
+        depth = case (depthNeeded w, w≡h && h≡d) of
+                  (Just requiredDepth, True) → requiredDepth
+                  _                          → error(printf errmsg(show(w,h,d)))
+
+heightMap ∷ HeightMap D → Cube
+heightMap = voxelPattern . pillars
+
+sauerbraten ∷ Cube → SauerbratenMap
+sauerbraten = roomOGZ . treeOctN
 
 
 -- Operations ------------------------------------------------------------------
 
-noProps ∷ Properties
-noProps = Properties Nothing $ Faces $ Tup6 Nothing Nothing Nothing
-                                            Nothing Nothing Nothing
+noProps ∷ Sauer.Properties
+noProps = Sauer.Properties Nothing $ Sauer.Faces $
+            Tup6 Nothing Nothing Nothing
+                 Nothing Nothing Nothing
 
-solid ∷ Octree
-solid = NSolid (Textures (Tup6 2 3 4 5 6 7)) noProps Nothing
+nSolid ∷ Sauer.Octree
+nSolid = Sauer.NSolid (Sauer.Textures (Tup6 2 3 4 5 6 7)) noProps Nothing
 
-empty ∷ Octree
-empty = NEmpty (Textures (Tup6 0 0 0 0 0 0)) noProps Nothing
+nEmpty ∷ Sauer.Octree
+nEmpty = Sauer.NEmpty (Sauer.Textures (Tup6 0 0 0 0 0 0)) noProps Nothing
 
-pillars ∷ HeightMap2D → BitField3d D
+nByNByN ∷ Int → DIM3
+nByNByN n = Z :. n :. n :. n
+
+pillars ∷ HeightMap D → BitField3D D
 pillars m = A.fromFunction (nByNByN size) translate
   where (Z:.w:.h, f) = A.toFunction m
         size = if w==h then w else error "Non-square heightmap"
@@ -77,79 +126,21 @@ indexTree depth = recurse depth (Z:.0:.0:.0)
             (recurse (d-1) $ Z :. x   :. y+1 :. z  )
             (recurse (d-1) $ Z :. x+1 :. y+1 :. z  )
 
-leaves ∷ Tree a → [a]
-leaves = f
-  where f (Leaf x) = [x]
-        f (Branch xs) = concat $ toList $ f <$> xs
+treeOctN ∷ Tree Bool → Sauer.Octree
+treeOctN (Leaf False) = nEmpty
+treeOctN (Leaf True)  = nSolid
+treeOctN (Branch b)   = Sauer.NBroken $ treeOctN <$> b
 
-maze3d ∷ BitField3d D → Tree Bool
-maze3d bf = f <$> indexTree depth
-  where (Z:.w:.h:.d, f) = A.toFunction bf
-        errmsg = "3d bit fields must be cubes with sizes that are powers of two. Not %s"
-        depth = case (depthNeeded w, w≡h && h≡d) of
-                  (Just requiredDepth, True) → requiredDepth
-                  _                          → error(printf errmsg(show(w,h,d)))
-
-maze2d ∷ BitField2d D → Tree Bool
-maze2d bf = f3 <$> indexTree depth
-  where (Z:.w:.h, f2) = A.toFunction bf
-        f3 (Z :. x :. y :. z) | z≡bottom = f2 (Z :. x :. y)
-        f3 (Z :. _ :. _ :. z)            = traceShow z False
-        bottom = (depth*depth) - 1
-        depth = traceShowId $ traceShowId $ traceShowId $
-          case (depthNeeded w, w≡h) of
-            (Just d, True) → d
-            _              → error
-              "2d bit fields must be squares with sizes that arepowers of two."
-
-squareRoot :: Int -> Int
-squareRoot = trace "squareRoot" . traceShowId . f . traceShowId
-  where f n = case n of
-          0 → 0
-          1 → 1
-          _ → let (^!) :: Num a => a -> Int -> a
-                  (^!) x y = x^y
-                  twopows = L.iterate (^!2) 2
-                  (lowerRoot, lowerN) =
-                     L.last $ takeWhile ((n>=) . snd) $ zip (1:twopows) twopows
-                  newtonStep x = div (x + div n x) 2
-                  iters = L.iterate newtonStep (f (div n lowerN) * lowerRoot)
-                  isRoot r  =  r^!2 <= n && n < (r+1)^!2
-              in  L.head $ L.dropWhile (not . isRoot) iters
-
-maze2d' ∷ [Int] → Octree
-maze2d' ls = treeOctN $ maze2d $ A.delay $ A.fromListUnboxed shape $ (≠0) <$> ls
-  where shape = Z :. size :. size
-        size = squareRoot $ traceShowId $ length ls
-
-stripes ∷ [Int]
-stripes = x4 $ x4 $ x4 [0,0,1,1]
-  where x4 a = mconcat[a,a,a,a]
-
-treeOctN ∷ Tree Bool → Octree
-treeOctN (Leaf False) = empty
-treeOctN (Leaf True)  = solid
-treeOctN (Branch b)   = NBroken $ treeOctN <$> b
-
-roomOGZ ∷ Octree → OGZ
+roomOGZ ∷ Sauer.Octree → SauerbratenMap
 roomOGZ geom =
-   OGZ (WorldSize 10)
-       [OGZVar "skybox" (OStr "ik2k/env/iklake")]
-       (GameType "fps")
-       (Extras 0 0)
-       (TextureMRU [2,4,3,5,7])
-       [Entity (Vec3 (Tup3 520.0 520.0 516.0)) (Tup5 0 0 0 0 0) PlayerStart 0]
-       (LzTup8 solid solid solid solid solid solid solid geom)
-
-nByN ∷ Int → DIM2
-nByN n = (Z :. n) :. n
-
-nByNByN ∷ Int → DIM3
-nByNByN n = Z :. n :. n :. n
-
-heightMap ∷ Noise a => a -> Int -> HeightMap2D
-heightMap nf size = A.fromFunction (nByN size) (toHeight . noiseValue nf . fromIdx)
-  where fromIdx ∷ (Z :. Int :. Int) → (Double,Double,Double)
-        fromIdx ((Z :. a) :. b) = (fromIntegral a, fromIntegral b, 0)
-        toHeight ∷ Double →  Int
-        toHeight noiseVal = floor $ (fromIntegral size) * ((noiseVal+1)/2.0)
+  Sauer.OGZ
+    (Sauer.WorldSize 10)
+    [Sauer.OGZVar "skybox" (Sauer.OStr "ik2k/env/iklake")]
+    (Sauer.GameType "fps")
+    (Sauer.Extras 0 0)
+    (Sauer.TextureMRU [2,4,3,5,7])
+    [Sauer.Entity (Sauer.Vec3 (Tup3 520.0 520.0 516.0))
+                  (Tup5 0 0 0 0 0)
+                  Sauer.PlayerStart
+                  0]
+    (LzTup8 nEmpty nEmpty nEmpty nEmpty nEmpty nEmpty nEmpty geom)
--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,46 @@
 module Main where
 
-import qualified Data.Array.Repa as A
+import           Control.Applicative
+import           Data.Array.Repa      ((:.) (..), D, DIM2, Z (Z))
+import qualified Data.Array.Repa      as A
+import qualified Data.List            as L
 import           Debug.Trace
 import qualified Gen
 import           Mapfile
-import           System.Random
+import           Numeric.Noise
 import           Numeric.Noise.Perlin
+import           Prelude.Unicode
+import           System.Random
+
+nByN ∷ Int → DIM2
+nByN n = (Z :. n) :. n
+
+genTerrain ∷ Noise a => a -> Int -> Gen.HeightMap D
+genTerrain nf size = A.fromFunction (nByN size) (toHeight . noiseValue nf . fromIdx)
+  where fromIdx ∷ (Z :. Int :. Int) → (Double,Double,Double)
+        fromIdx ((Z :. a) :. b) = (fromIntegral a, fromIntegral b, 0)
+        toHeight ∷ Double →  Int
+        toHeight noiseVal = floor $ fromIntegral size * ((noiseVal+1)/2.0)
+
+squareRoot :: Int -> Int
+squareRoot = f
+  where f n = case n of
+          0 → 0
+          1 → 1
+          _ → let (^!) :: Num a => a -> Int -> a
+                  (^!) x y = x^y
+                  twopows = L.iterate (^!2) 2
+                  (lowerRoot, lowerN) =
+                     L.last $ takeWhile ((n>=) . snd) $ zip (1:twopows) twopows
+                  newtonStep x = div (x + div n x) 2
+                  iters = L.iterate newtonStep (f (div n lowerN) * lowerRoot)
+                  isRoot r  =  r^!2 <= n && n < (r+1)^!2
+              in  L.head $ L.dropWhile (not . isRoot) iters
+
+maze ∷ [Int] → Gen.BitField2D D
+maze ls = A.delay $ A.fromListUnboxed shape $ (≠0) <$> ls
+  where shape = Z :. size :. size
+        size = squareRoot $ length ls
 
 genMaps ∷ IO ()
 genMaps = do
@@ -20,27 +55,26 @@ genMaps = do
     let persistance = 0.5
     let perlinNoise = perlin seed octaves scale persistance
 
-    let foobarzaz'  = Gen.heightMap perlinNoise 16
+    let foobarzaz'  = genTerrain perlinNoise 16
     let _foobarzaz   = A.toList foobarzaz'
 
-    let aHMap = Gen.heightMap perlinNoise 32
-    let aBitField = Gen.pillars aHMap
-    let a3dMaze = Gen.maze3d aBitField
-    let terrain = Gen.roomOGZ $ Gen.treeOctN a3dMaze
+    let aHMap = genTerrain perlinNoise 32
+    let a3dMaze = Gen.heightMap aHMap
+    let terrain = Gen.sauerbraten a3dMaze
 
     g ← getStdGen
     let foo = take 256 $ randomRs (0,1) g
 
+    traceM "<Generating map=\"generated.ogz\">"
     traceM $ show foo
-    let m = Gen.roomOGZ . Gen.maze2d' $ foo
+    let m = Gen.sauerbraten . Gen.floorPattern . maze $ foo
 
-    traceM "<Writing to generated.ogz>"
     dumpOGZ outfile m
-    traceM "</Writing to generated.ogz>"
+    traceM "</Generating>"
 
-    traceM "<Writing to genterrain.ogz>"
+    traceM "<Generating map=\"genterrain.ogz\">"
     dumpOGZ outfile2 terrain
-    traceM "</Writing to genterrain.ogz>"
+    traceM "</Generating>"
 
 main ∷ IO ()
 main = do
